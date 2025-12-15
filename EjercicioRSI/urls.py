6aeae8f30f12c7c8d7d473c8a3067c06345df0f8
Lucía Ponce García de Sola <114416598+luciaponce-us@users.noreply.github.com>
2025-12-15 13:36:53 +0100

    Nuevas migraciones

--- EjercicioRSI/urls.py
+++ EjercicioRSI/urls.py
@@ -6,8 +6,5 @@ urlpatterns = [
     path('',views.index),
     path('index.html/', views.index),
     path('populate/', views.populateDatabase),
-    path('loadRS/', views.loadRS),
-    path('ingresar/', views.ingresar),    
-    path('admin/',admin.site.urls),
     path('animes_por_formato/', views.animes_por_formato, name='animes_por_formato'),
     ]
